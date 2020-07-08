cea206a85ac325266c64f0035a2ef46bb674ad34
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-08 21:00:35 +0200

    Style banner

--- components/Banner.js
+++ components/Banner.js
@@ -18,10 +18,13 @@ const Banner = (props) => {
   return (
     <div className="Banner">
       <Grid container spacing={3}>
-        <Grid item xs={6}>
-          text
+        <Grid item lg={6} sm={12}>
+          <p className="Banner__text Banner__text-bold">Naszą misją jest usprawnienie komunikacji na linii lekarz-pacjent.</p>
+          <p className="Banner__text">Tutaj zawodowi lekarze tłumaczą terminy medyczne na zrozumiały język. Tak po prostu.</p>
+          <p className="Banner__text"><span className="Banner__text-bold">Jesteś pacjentem?</span> Masz dostęp do rzetelnych, sprawdzonych informacji by lepiej zrozumieć skomplikowane rezultaty badań, wyniki konsultacji i wyisy ze szpitala.</p>
+          <p className="Banner__text"><span className="Banner__text-bold">Jesteś lekarzem?</span> Masz pod ręką wygodne narzędzie, które pomaga sprawnie wytłumaczyć pacjentowi zaawiłe pojęcia.</p>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item lg={6} sm={12}>
           <div>
             <img className="Banner__image" alt="alt" src="/images/banner.jpg"/>
           </div>
