1078386d625afa2ce50bdcd01b3f1eca9d6dd7c4
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-08 20:09:31 +0200

    Capture form input

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -4,20 +4,57 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from "@material-ui/core/TextField";
 
 const SearchForm = (props) => {
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(event.target);
+  const [value, setValue] = React.useState(null);
+
+  const onSubmit = event => {
+    event.preventDefault();
+    navigateToTerm(value.name)
+  }
+
+  const handleClick = () => {
+    navigateToTerm(value.name)
+  }
+
+  const navigateToTerm = (selectedName) => {
+    console.log(selectedName)
   }
 
   return (
     <div className="SearchForm">
-      <h1 className="SearchForm__title">Tłumaczymy trudne pojęcia medyczne na ludzki</h1>
-      <form onSubmit={handleSubmit}>
+      <h1 className="SearchForm__title">Sprawdź, co lekarz miał na myśli</h1>
+      <form onSubmit={onSubmit}>
         <Autocomplete
+          value={value}
+          onChange={(event, newValue) => {
+            if (typeof newValue === 'string') {
+              setValue({
+                name: newValue,
+              });
+            } else if (newValue && newValue.inputValue) {
+              // Create a new value from the user input
+              setValue({
+                name: newValue.inputValue,
+              });
+            } else {
+              setValue(newValue);
+            }
+          }}
           className="SearchForm__input"
           freeSolo
           id="free-solo-2-demo"
           disableClearable
+          getOptionLabel={(option) => {
+            // Value selected with enter, right from the input
+            if (typeof option === 'string') {
+              return option;
+            }
+            // Add "xxx" option created dynamically
+            if (option.inputValue) {
+              return option.inputValue;
+            }
+            // Regular option
+            return option.name;
+          }}
           options={props.terms.map((option) => option.name)}
           renderInput={(params) => (
             <TextField
@@ -29,7 +66,7 @@ const SearchForm = (props) => {
           )}
         />
       </form>
-      <button type="submit" className="SearchForm__buttonPrimary">Szukaj</button>
+      <button onClick={handleClick} type="submit" className="SearchForm__buttonPrimary">Szukaj</button>
       <div className="SearchForm__otherTerms">
         <div className="SearchForm__or">lub</div>
         <button className="SearchForm__buttonSecondary">sprawdź najczęściej wyszukiwane hasła</button>
