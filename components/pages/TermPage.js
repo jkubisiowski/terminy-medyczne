7baf6df0e4763ee52caf8386a664a1c3d0d20649
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-09 20:54:49 +0200

    Create author card

--- components/pages/TermPage.js
+++ components/pages/TermPage.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import BlockContent from "@sanity/block-content-to-react";
 import client from "../../client";
 import imageUrlBuilder from "@sanity/image-url";
 import TermCard from "../TermCard";
+import AuthorCard from "../AuthorCard";
 
 function urlFor(source) {
   return imageUrlBuilder(client).image(source)
@@ -20,28 +20,7 @@ const TermPage = (props) => {
   return (
     <>
       <TermCard categories={categories} name={name} body={body}/>
-      {/*<h1>{name}</h1>*/}
-      {/*<span>By {authorName}</span>*/}
-      {/*{categories && (*/}
-      {/*  <ul>*/}
-      {/*    Posted in*/}
-      {/*    {categories.map(category => <li key={category}>{category}</li>)}*/}
-      {/*  </ul>*/}
-      {/*)}*/}
-      {/*{authorImage && (*/}
-      {/*  <div>*/}
-      {/*    <img*/}
-      {/*      src={urlFor(authorImage)*/}
-      {/*        .width(50)*/}
-      {/*        .url()}*/}
-      {/*      alt={authorImage}/>*/}
-      {/*  </div>*/}
-      {/*)}*/}
-      {/*<BlockContent*/}
-      {/*  blocks={body}*/}
-      {/*  imageOptions={{w: 320, h: 240, fit: 'max'}}*/}
-      {/*  {...client.config()}*/}
-      {/*/>*/}
+      <AuthorCard authorName={authorName} authorImage={authorImage}/>
     </>
   )
 }
