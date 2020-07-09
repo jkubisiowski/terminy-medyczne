2d78df0e06eca0b6268c96022e054bea3454b7a2
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-09 20:49:01 +0200

    Create term card

---
+++ components/TermCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./TermCard.scss"
+import client from "../client";
+import BlockContent from "@sanity/block-content-to-react";
+
+const TermCard = (props) => {
+  return (
+    <div className="TermCard">
+      {props.categories && (
+        <ul>
+          {props.categories.map(category => <li key={category}>{category}</li>)}
+        </ul>
+      )}
+      <h2>{props.name}</h2>
+      <BlockContent
+        blocks={props.body}
+        imageOptions={{w: 320, h: 240, fit: 'max'}}
+        {...client.config()}
+      />
+    </div>
+  )
+}
+
+export default TermCard
--- components/pages/TermPage.js
+++ components/pages/TermPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import BlockContent from "@sanity/block-content-to-react";
 import client from "../../client";
 import imageUrlBuilder from "@sanity/image-url";
+import TermCard from "../TermCard";
 
 function urlFor(source) {
   return imageUrlBuilder(client).image(source)
@@ -16,30 +17,33 @@ const TermPage = (props) => {
     body = []
   } = props
 
-  return (<article>
-    <h1>{name}</h1>
-    <span>By {authorName}</span>
-    {categories && (
-      <ul>
-        Posted in
-        {categories.map(category => <li key={category}>{category}</li>)}
-      </ul>
-    )}
-    {authorImage && (
-      <div>
-        <img
-          src={urlFor(authorImage)
-            .width(50)
-            .url()}
-          alt={authorImage}/>
-      </div>
-    )}
-    <BlockContent
-      blocks={body}
-      imageOptions={{w: 320, h: 240, fit: 'max'}}
-      {...client.config()}
-    />
-  </article>)
+  return (
+    <>
+      <TermCard categories={categories} name={name} body={body}/>
+      {/*<h1>{name}</h1>*/}
+      {/*<span>By {authorName}</span>*/}
+      {/*{categories && (*/}
+      {/*  <ul>*/}
+      {/*    Posted in*/}
+      {/*    {categories.map(category => <li key={category}>{category}</li>)}*/}
+      {/*  </ul>*/}
+      {/*)}*/}
+      {/*{authorImage && (*/}
+      {/*  <div>*/}
+      {/*    <img*/}
+      {/*      src={urlFor(authorImage)*/}
+      {/*        .width(50)*/}
+      {/*        .url()}*/}
+      {/*      alt={authorImage}/>*/}
+      {/*  </div>*/}
+      {/*)}*/}
+      {/*<BlockContent*/}
+      {/*  blocks={body}*/}
+      {/*  imageOptions={{w: 320, h: 240, fit: 'max'}}*/}
+      {/*  {...client.config()}*/}
+      {/*/>*/}
+    </>
+  )
 }
 
 export default TermPage
