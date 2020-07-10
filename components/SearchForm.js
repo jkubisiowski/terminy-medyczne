99752c4e7db20726937f05b457c9ae34cf980b07
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-10 15:41:55 +0200

    Move search form to different component

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -8,76 +8,64 @@ const SearchForm = (props) => {
 
   const onSubmit = event => {
     event.preventDefault();
-    if(value!== null)
-      navigateToTerm(value.name)
-  }
-
-  const handleClick = () => {
-    if(value!== null)
+    if (value !== null)
       navigateToTerm(value.name)
   }
 
   const navigateToTerm = (selectedName) => {
     const found = props.terms.find(x => x.name === selectedName);
-    if(found !== null){
+    if (found !== null) {
       console.log(found)
       window.location.href = "/term/" + found.slug.current;
     }
   }
 
   return (
-    <div className="SearchForm">
-      <h1 className="SearchForm__title">Sprawdź, co lekarz miał na myśli</h1>
-      <form onSubmit={onSubmit}>
-        <Autocomplete
-          value={value}
-          onChange={(event, newValue) => {
-            if (typeof newValue === 'string') {
-              setValue({
-                name: newValue,
-              });
-            } else if (newValue && newValue.inputValue) {
-              // Create a new value from the user input
-              setValue({
-                name: newValue.inputValue,
-              });
-            } else {
-              setValue(newValue);
-            }
-          }}
-          className="SearchForm__input"
-          freeSolo
-          disableClearable
-          getOptionLabel={(option) => {
-            // Value selected with enter, right from the input
-            if (typeof option === 'string') {
-              return option;
-            }
-            // Add "xxx" option created dynamically
-            if (option.inputValue) {
-              return option.inputValue;
-            }
-            // Regular option
-            return option.name;
-          }}
-          options={props.terms.map((option) => option.name)}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Wpisz termin medyczny"
-              variant="outlined"
-              InputProps={{...params.InputProps, type: 'search'}}
-            />
-          )}
-        />
-      </form>
-      <button onClick={handleClick} type="submit" className="SearchForm__buttonPrimary">Szukaj</button>
-      <div className="SearchForm__otherTerms">
-        <div className="SearchForm__or">lub</div>
-        <button className="SearchForm__buttonSecondary">sprawdź najczęściej wyszukiwane hasła</button>
-      </div>
+    <form className="SearchForm" onSubmit={onSubmit}>
+      <Autocomplete
+        value={value}
+        onChange={(event, newValue) => {
+          if (typeof newValue === 'string') {
+            setValue({
+              name: newValue,
+            });
+          } else if (newValue && newValue.inputValue) {
+            // Create a new value from the user input
+            setValue({
+              name: newValue.inputValue,
+            });
+          } else {
+            setValue(newValue);
+          }
+        }}
+        className="SearchForm__input"
+        freeSolo
+        disableClearable
+        getOptionLabel={(option) => {
+          // Value selected with enter, right from the input
+          if (typeof option === 'string') {
+            return option;
+          }
+          // Add "xxx" option created dynamically
+          if (option.inputValue) {
+            return option.inputValue;
+          }
+          // Regular option
+          return option.name;
+        }}
+        options={props.terms.map((option) => option.name)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Wpisz termin medyczny"
+            variant="outlined"
+            InputProps={{...params.InputProps, type: 'search'}}
+          />
+        )}
+      />
+      <button type="submit" className="SearchForm__button">Szukaj</button>
+    </form>
 
-    </div>
   )
 }
 
