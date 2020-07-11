b5ec01648619485403d59ee62f65483f2bdf4504
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-11 12:42:43 +0200

    Add key in frequent terms

--- components/FrequentTerms.js
+++ components/FrequentTerms.js
@@ -4,7 +4,7 @@ import "./FrequentTerms.scss"
 const FrequentTerms = (props) => {
   return (
     <div className="FrequentTerms">
-      {props.terms.map(x => <div className="FrequentTerms__itemContainer">
+      {props.terms.map(x => <div key={x.name} className="FrequentTerms__itemContainer">
         <a className="FrequentTerms__item" href={x.url}>{x.name}</a>
       </div>)}
     </div>
