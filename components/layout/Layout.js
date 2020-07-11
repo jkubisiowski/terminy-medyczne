115a8b3dd701f22e5d74a8c9f9e5c3b393ab8384
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-11 14:07:58 +0200

    Add layout folders

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -8,16 +8,19 @@ function Layout(props) {
   return (
     <>
       <Head>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width, viewport-fit=cover'/>
+        <meta charSet="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
         <title>Terminy medyczne</title>
+        <link rel="stylesheet" href="./plugins/bootstrap-4.3.1/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="./plugins/meanmenu/meanmenu.css"/>
+        <link rel="stylesheet" href="./fonts/ep-icon-fonts/css/style.css"/>
+        <link rel="stylesheet" href="./fonts/fontawesome-5/css/all.min.css"/>
+        <link rel="stylesheet" href="./css/settings.css"/>
+        <link rel="stylesheet" href="./css/style.css"/>
       </Head>
       <div id="page-container">
-        <Header/>
-        <Navigation/>
-        <div id="content-wrap">{props.children}</div>
-        <div id="footer">
-          <Footer/>
-        </div>
+       Lol
       </div>
     </>
   )
