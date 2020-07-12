8d883f55d58fd96fa4a6cc49cc97fc993ce95a17
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-12 10:35:57 +0200

    Fix styling

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,12 @@ const Index = (props) => {
       <section className="hero-area">
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-lg-6 order-lg-2">
+            <div className="col-lg-5 order-lg-2">
               <div className="hero-image">
                 <img src="/image/main-image.png" alt=""/>
               </div>
             </div>
-            <div className="col-lg-6 order-lg-1">
+            <div className="col-lg-7 order-lg-1">
               <div className="hero-content">
                 <h2>Sprawdź, co lekarz miał na myśli</h2>
                 <p>Tutaj zawodowi lekarze tłumaczą terminy medyczne na zrozumiały język. Tak po prostu.
