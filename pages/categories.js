8fdfe0de43e461a7a0894c3cb3356723d8299887
karolina <karolina@users-MacBook-Air.local>
2020-07-13 20:54:34 +0200

    Add categories

---
+++ pages/categories.js
@@ -0,0 +1,58 @@
+import Layout from "../components/Layout";
+
+const Categories = () => {
+    return (
+        <Layout>
+            <section className="hero-area term">
+            <div className="container">
+            <div className="section-title text-center">
+              <h2>Kategorie</h2>
+            </div>
+            <div className="row mb-d-30">
+              <div className="col-md-6 col-lg-3 mb--30">
+                <div className="feature-card">
+                  <div className="card-content">
+                    <h4>Dupa 1</h4>
+                    <a href="" className="btn btn-link right-icon">Sprawdź tutaj <i
+                      className="icon icon-minimal-right"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-6 col-lg-3 mb--30">
+                <div className="feature-card">
+                  <div className="card-content">
+                    <h4>Dupa 2</h4>
+                    <a href="" className="btn btn-link right-icon">Sprawdź tutaj <i
+                      className="icon icon-minimal-right"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-6 col-lg-3 mb--30">
+                <div className="feature-card">
+                  <div className="card-content">
+                    <h4>Dupa 3</h4>
+                    <a href="" className="btn btn-link right-icon">Sprawdź tutaj <i
+                      className="icon icon-minimal-right"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-3 mb--30">
+                <div className="feature-card">
+                  <div className="card-content">
+                    <h4>Dupa 4</h4>
+                    <a href="" className="btn btn-link right-icon">Sprawdź tutaj <i
+                      className="icon icon-minimal-right"></i></a>
+                  </div>
+                </div>
+              </div>
+                </div>
+            </div>
+            </section>
+        </Layout>
+    )
+  }
+  
+  export default Categories;
+  
