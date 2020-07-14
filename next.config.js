828b18d8016fc92d2e122c845f72a36cbcf16fe5
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-14 23:25:18 +0200

    Disable etag generation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const client = require('./client')
 
 module.exports = {
+  generateEtags: false,
   exportPathMap: async function (defaultPathMap) {
     const paths = await client
       .fetch('*[_type == "term" && defined(slug)].slug.current')
