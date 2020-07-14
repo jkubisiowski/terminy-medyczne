0776f5586c5092252fc576f0750cc8b65f699691
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-14 22:16:16 +0200

    Don't user SSR for autocomplete

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,6 +1,6 @@
-// import "./SearchForm.scss"
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from "@material-ui/core/TextField";
+import NoSsr from "@material-ui/core/NoSsr";
 
 const SearchForm = (props) => {
   const [value, setValue] = React.useState(null);
@@ -22,46 +22,48 @@ const SearchForm = (props) => {
   return (
     <form className="SearchForm" onSubmit={onSubmit}>
       <div className="">
-        <Autocomplete
-          value={value}
-          onChange={(event, newValue) => {
-            if (typeof newValue === 'string') {
-              setValue({
-                name: newValue,
-              });
-            } else if (newValue && newValue.inputValue) {
-              // Create a new value from the user input
-              setValue({
-                name: newValue.inputValue,
-              });
-            } else {
-              setValue(newValue);
-            }
-          }}
-          freeSolo
-          disableClearable
-          getOptionLabel={(option) => {
-            // Value selected with enter, right from the input
-            if (typeof option === 'string') {
-              return option;
-            }
-            // Add "xxx" option created dynamically
-            if (option.inputValue) {
-              return option.inputValue;
-            }
-            // Regular option
-            return option.name;
-          }}
-          options={props.terms.map((option) => option.name)}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Wpisz termin medyczny"
-              variant="outlined"
-              InputProps={{...params.InputProps, type: 'search'}}
-            />
-          )}
-        />
+        <NoSsr>
+          <Autocomplete
+            value={value}
+            onChange={(event, newValue) => {
+              if (typeof newValue === 'string') {
+                setValue({
+                  name: newValue,
+                });
+              } else if (newValue && newValue.inputValue) {
+                // Create a new value from the user input
+                setValue({
+                  name: newValue.inputValue,
+                });
+              } else {
+                setValue(newValue);
+              }
+            }}
+            freeSolo
+            disableClearable
+            getOptionLabel={(option) => {
+              // Value selected with enter, right from the input
+              if (typeof option === 'string') {
+                return option;
+              }
+              // Add "xxx" option created dynamically
+              if (option.inputValue) {
+                return option.inputValue;
+              }
+              // Regular option
+              return option.name;
+            }}
+            options={props.terms.map((option) => option.name)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Wpisz termin medyczny"
+                variant="outlined"
+                InputProps={{...params.InputProps, type: 'search'}}
+              />
+            )}
+          />
+        </NoSsr>
         <button type="submit" className="search-form-submit">Szukaj</button>
       </div>
 
