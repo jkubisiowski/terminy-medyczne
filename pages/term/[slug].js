6d6d5f06962451b461812dfa0aa4e97a9a5fa388
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-15 19:24:06 +0200

    Load term data on client side

--- pages/term/[slug].js
+++ pages/term/[slug].js
@@ -1,11 +1,26 @@
 import groq from 'groq'
 import client from '../../client'
 import Layout from "../../components/Layout";
-// import "./TermPage.scss"
 import imageUrlBuilder from '@sanity/image-url'
 import BlockContent from '@sanity/block-content-to-react'
+import {useEffect, useState} from "react";
 
 const TermPage = (props) => {
+  const [term, setTerm] = useState({
+    body: [],
+    name: null,
+    authorImage: null,
+    authorName: null,
+    categories: []
+  });
+
+  useEffect(() => {
+    client.fetch(query, {slug: props.slug})
+      .then(data => {
+        setTerm(data)
+      })
+  }, [])
+
   return (
     <Layout>
       <section className="hero-area term">
@@ -14,39 +29,38 @@ const TermPage = (props) => {
             <div className="col-lg-4 order-2">
               <div>
                 <p className="term-author-label">Autor tłumaczenia</p>
-                <h5 className="term-author-name">{props.authorName}</h5>
-                {props.authorImage && (
+                <h5 className="term-author-name">{term.authorName}</h5>
+                {term.authorImage && (
                   <div>
                     <img
-                      src={urlFor(props.authorImage)
+                      src={urlFor(term.authorImage)
                         .width(200)
                         .url()}
-                      alt={props.authorImage}/>
+                      alt={term.authorImage}/>
                   </div>
                 )}
               </div>
             </div>
             <div className="col-lg-8 order-1">
               <div className="hero-content">
-                <h2>{props.name}</h2>
+                <h2>{term.name}</h2>
                 <div className="term-body">
                   <BlockContent
                     className="term-body"
-                    blocks={props.body}
+                    blocks={term.body}
                     imageOptions={{w: 320, h: 240, fit: 'max'}}
                     {...client.config()}
                   />
                 </div>
-                {props.categories && (
+                {term.categories && (
                   <ul>
-                    {props.categories.map(category => <li className="term-category-container" key={category}>
+                    {term.categories.map(category => <li className="term-category-container" key={category}>
                       <span>{category}</span></li>)}
                   </ul>
                 )}
               </div>
             </div>
           </div>
-
         </div>
       </section>
     </Layout>
@@ -65,12 +79,8 @@ const query = groq`*[_type == "term" && slug.current == $slug][0]{
   body
 }`
 
-TermPage.getInitialProps = async function (context) {
-  // It's important to default the slug so that it doesn't return "undefined"
-  const {slug = ""} = context.query
-
-  // const terms = await client.fetch(groq`*[_type == "term"]`)
-  return await client.fetch(query, {slug});
+TermPage.getInitialProps = function (context) {
+  return context.query;
 }
 
 export default TermPage
