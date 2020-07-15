96406bf7b60a440510ca7269afaa3f34e3026d47
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-15 21:03:15 +0200

    Fix paths

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const client = require('./client')
 module.exports = {
   generateEtags: false,
   exportPathMap: async function (defaultPathMap) {
-    const paths = await client
+    const pathsTerms = await client
       .fetch('*[_type == "term" && defined(slug)].slug.current')
       .then(data =>
         data.reduce(
@@ -30,7 +30,7 @@ module.exports = {
       )
       .catch(console.error)
     return {
-      paths, pathsCategories
+      ...pathsTerms, ...pathsCategories
     }
   }
 }
