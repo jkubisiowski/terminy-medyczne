e975169f6702d34f340c44f005adbfaacd579c71
karolina <karolina@users-MacBook-Air.local>
2020-07-21 14:55:10 +0200

    Add Sprawdź termin in navbar

--- components/Layout.js
+++ components/Layout.js
@@ -34,25 +34,26 @@ function Layout(props) {
       <div className="site-wrapper">
         <header className="site-header">
           <div className="container">
-            <div className="row justify-content-center align-items-center position-relative">
+            <div className="row justify-content-left align-items-center position-relative">
               <div className="col-sm-3 col-6 col-lg-2 col-xl-2 order-lg-1">
                 <div className="brand">
                   <a href="/"><img src="/image/main-logo.png" alt=""/></a>
                 </div>
               </div>
-              <div className="col-sm-8 col-lg-3 col-xl-3 d-none d-sm-block order-lg-3">
+              {/* <div className="col-sm-8 col-lg-3 col-xl-3 d-none d-sm-block order-lg-3">
                 <div className="header-btns">
-                  {/* <div className="btn-1">
+                  <div className="btn-1">
                     <a href="#">Sign In</a>
-                  </div> */}
+                  </div>
                   <div className="btn-2">
                     <a href="/">Szukaj</a>
                   </div>
                 </div>
-              </div>
+              </div> */}
               <div className="col-sm-1 col-6 col-lg-7 col-xl-6 offset-xl-1 position-static order-lg-2">
                 <div className="main-navigation">
                   <ul className="main-menu">
+                    <li className="menu-item"><a href="/">Sprawdź termin</a></li>
                     <li className="menu-item"><a href="#about-us">O nas</a></li>
                     <li className="menu-item"><a href="#about-project">O projekcie</a></li>
                     <li className="menu-item"><a href="/kategorie">Kategorie</a></li>
