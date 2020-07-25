24d1b18f821bc9eac3003cdcc99aa1f2f62cb68c
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-25 10:26:28 +0200

    Add dynamic routes

--- next.config.js
+++ next.config.js
@@ -2,15 +2,16 @@ const client = require('./client')
 
 module.exports = {
   generateEtags: true,
-  exportPathMap: async function (defaultPathMap) {
+  exportPathMap: async function (defaultPathMap,
+    {dev, dir, outDir, distDir, buildId}) {
     const pathsTerms = await client
       .fetch('*[_type == "term" && defined(slug)].slug.current')
       .then(data =>
         data.reduce(
           (acc, slug) => ({
-            '/': { page: '/' },
+            '/': {page: '/'},
             ...acc,
-            [`/terminy/${slug}`]: { page: '/terminy/[slug]', query: { slug } }
+            [`/terminy/${slug}`]: {page: '/terminy/[slug]', query: {slug}}
           }),
           defaultPathMap
         )
@@ -21,15 +22,16 @@ module.exports = {
       .then(data =>
         data.reduce(
           (acc, slug) => ({
-            '/': { page: '/' },
+            '/': {page: '/'},
             ...acc,
-            [`/kategorie/${slug}`]: { page: '/kategorie/[slug]', query: { slug } }
+            [`/kategorie/${slug}`]: {page: '/kategorie/[slug]', query: {slug}}
           }),
           defaultPathMap
         )
       )
       .catch(console.error)
     return {
+
       ...pathsTerms, ...pathsCategories
     }
   }
