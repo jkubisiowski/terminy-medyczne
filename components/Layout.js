00345e0d43f8bf217c5825b4941dd6edfe1b1fe3
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-07-29 21:56:03 +0200

    Remove unnecessary code

--- components/Layout.js
+++ components/Layout.js
@@ -178,17 +178,10 @@ function Layout(props) {
                     <b>Jesteś lekarzem?</b> Masz pod ręką wygodne narzędzie, które pomaga sprawnie wytłumaczyć
                     pacjentowi zawiłe pojęcia.
                   </p>
-                  {/* <div className="content-bottom-part">
-                    <p></p>
-                    <div className="content-btn">
-                      <a href="#" className="btn btn--primary">Schedule A Demo</a>
-                    </div>
-                  </div> */}
                 </div>
               </div>
             </div>
           </div>
-
         </section>
 
         <section id="kontakt" className="pricing-section">
@@ -231,138 +224,6 @@ function Layout(props) {
             </div>
           </div>
         </section>
-        {/* <section className="pricing-section">
-          <div className="shape">
-            <img src="/image/landing-2-shape.svg" alt=""/>
-          </div>
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-lg-8">
-                <div className="section-title text-center">
-                  <h2>Mark your next step</h2>
-                  <p>We designed and tested prototypes that helped identify pain points in the account creation process.
-                    Together, we shaped the new standard.</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-12">
-                <div className="btn--toggle-pricing" data-tab-target="#pricing-tab-7">
-                  <p>Monthly</p>
-                  <label className="switch">
-                    <input type="checkbox"/>
-                      <span className="slider round"></span>
-                  </label>
-                  <p>Yearly</p>
-                </div>
-              </div>
-
-            </div>
-            <div className="custom-tab-wrapper monthly" id="pricing-tab-7">
-              <div className="row mb-d-30">
-                <div className="col-lg-4">
-                  <div className="pricing-card">
-
-                    <div className="card-head">
-                      <h6>Basic</h6>
-                      <h2><span className="small-title">$</span><span className="big-txt" data-monthly='19'
-                                                                      data-yearly='14'>19</span><span className="month"
-                                                                                                      data-monthly='/month'
-                                                                                                      data-yearly='/year'>/month</span>
-                      </h2>
-                      <p>Good for small business launching their products less then once a year</p>
-                    </div>
-
-                    <div className="card-content">
-                      <ul className="pricing-list">
-                        <li><span><i className="icon icon-check-2"></i></span>Unlimited Blocks</li>
-                        <li><span><i className="icon icon-check-2"></i></span>5GB Clould Storages</li>
-                        <li className="disable-opt"><span><i className="icon icon-check-2"></i></span>Custom Domain
-                          Names
-                        </li>
-                        <li className="disable-opt"><span><i className="icon icon-check-2"></i></span>Unlimited
-                          Emails
-                        </li>
-                      </ul>
-                      <div className="card-btn">
-                        <a href="" className="btn btn--primary">Start Free Trial</a>
-                      </div>
-                      <p>No credit card required</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-lg-4">
-                  <div className="pricing-card active">
-
-                    <div className="card-head">
-                      <h6>Pro</h6>
-                      <span className="card-badge">Popular</span>
-                      <h2><span className="small-title">$</span><span className="big-txt" data-monthly='29'
-                                                                      data-yearly='25'>29</span><span className="month"
-                                                                                                      data-monthly='/month'
-                                                                                                      data-yearly='/year'>/month</span>
-                      </h2>
-                      <p>Good for small business launching their products less then once a year</p>
-                    </div>
-
-
-                    <div className="card-content">
-                      <ul className="pricing-list">
-                        <li><span><i className="icon icon-check-2"></i></span>Unlimited Blocks</li>
-                        <li><span><i className="icon icon-check-2"></i></span>5GB Clould Storages</li>
-                        <li><span><i className="icon icon-check-2"></i></span>Custom Domain Names</li>
-                        <li className="disable-opt"><span><i className="icon icon-check-2"></i></span>Unlimited
-                          Emails
-                        </li>
-                      </ul>
-                      <div className="card-btn">
-                        <a href="" className="btn btn--primary">Start Free Trial</a>
-                      </div>
-                      <p>No credit card required</p>
-                    </div>
-                  </div>
-                </div>
-
-
-                <div className="col-lg-4">
-                  <div className="pricing-card">
-
-                    <div className="card-head">
-                      <h6>Exclusive</h6>
-                      <h2><span className="small-title">$</span><span className="big-txt" data-monthly='59'
-                                                                      data-yearly='49'>59</span><span className="month"
-                                                                                                      data-monthly='/month'
-                                                                                                      data-yearly='/year'>/month</span>
-                      </h2>
-                      <p>Good for small business launching their products less then once a year</p>
-                    </div>
-
-
-                    <div className="card-content">
-                      <ul className="pricing-list">
-                        <li><span><i className="icon icon-check-2"></i></span>Unlimited Blocks</li>
-                        <li><span><i className="icon icon-check-2"></i></span>5GB Clould Storages</li>
-                        <li><span><i className="icon icon-check-2"></i></span>Custom Domain Names</li>
-                        <li><span><i className="icon icon-check-2"></i></span>Unlimited Emails</li>
-                      </ul>
-                      <div className="card-btn">
-                        <a href="" className="btn btn--primary">Start Free Trial</a>
-                      </div>
-                      <p>No credit card required</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-12">
-                  <div className="pricing-extra-info">
-                    <p><span>Still confused?</span><a href="#">Check our full feature list</a></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section> */}
 
         <div className="footer-section">
           <div className="container">
@@ -391,13 +252,6 @@ function Layout(props) {
             </div> */}
 
             <div className="row margin-decrese">
-              {/* <div className="col-xl-4 col-lg-5 col-margin">
-                <div className="single-footer">
-                  <div className="footer-title">
-                    <img src="/image/footer-logo.png" alt=""/>
-                  </div>
-                  </div>
-                  </div> */}
                 <div className="col-lg-2 col-sm-4 col-6 col-margin">
                 <div className="single-footer">
                   <div className="footer-title">
